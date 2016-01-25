897acf6f96e6a1648b6432c5fd5dc9e9d5c734a3
anil614sagar <anil614sagar@gmail.com>
2016-01-25 10:52:10 +0530

    JIRA: PCFI-10 - Getting latest version & deploying same

--- cf-service-broker-api/api/api.js
+++ cf-service-broker-api/api/api.js
@@ -11,8 +11,6 @@ var auth = require('../helpers/auth')(config.get('cf_broker').auth.method)
 
 router.use(auth)
 router.get('/', function (req, res) {
-  // var conf = config.get()
-  // conf.apigee_edge.password = '********' TODO: mask the password. Can't do it this way.
   res.json({ message: 'This is the cf-apigee-broker CF Service Broker API.', configuration: 'blocked' })
 })
 
