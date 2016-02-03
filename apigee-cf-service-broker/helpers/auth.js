d1962759caad895ac93dbe9a4d62ccb303e2cbbd
anil614sagar <anil614sagar@gmail.com>
2016-02-03 18:55:54 +0530

    Remove Apigee Access, Vault related code

--- apigee-cf-service-broker/helpers/auth.js
+++ apigee-cf-service-broker/helpers/auth.js
@@ -117,51 +117,11 @@ var basicauth = function (req, res, next) {
   })
 }
 
-// internal call using apigee-access module (requires running in edge)
-// Not using below code anywhere
-/* istanbul ignore next */
-var apigeeauth = function (req, res, next) {
-  var apigee = require('apigee-access')
-  if (apigee.getMode() === apigee.APIGEE_MODE) {
-    var oauth = apigee.getOAuth()
-  } else {
-    throw new Error('Using internal apigee auth requires running in Apigee Edge.')
-  }
-
-  function unauthorized (res) {
-    res.set('WWW-Authenticate', 'Basic realm=Authorization Required')
-    return res.sendStatus(401)
-  }
-  if (!req.header('Authorization')) {
-    return unauthorized(res)
-  }
-  var user = basicAuth(req)
-  console.log('apikey: ' + user.name)
-  if (!oauth) {
-    return res.sendStatus(400)
-  }
-  // modify req to set correct patch
-
-  oauth.verifyApiKey(req, {apiKey: user.name}, function (err, result) {
-    if (err) {
-      return res.status(500).json(err)
-    } else {
-      if (result.error) return unauthorized(res)
-      if (result.attributes.client_secret === user.pass) {
-        return next()
-      } else {
-        return unauthorized(res)
-      }
-    }
-  })
-}
-
 module.exports = function (options) {
   var auths = {
     staticauth: staticauth,
     clientcredentials: clientcredentials,
     basicauth: basicauth,
-    apigeeauth: apigeeauth,
     anybasicauth: anybasicauth,
     apigeeuserauth: apigeeuserauth
   }
