f012493992741703287f9370690919932d2cd37e
anil614sagar <anil614sagar@gmail.com>
2016-02-03 18:53:00 +0530

    Removed commented code

--- apigee-cf-service-broker/helpers/config.js
+++ apigee-cf-service-broker/helpers/config.js
@@ -7,32 +7,4 @@ nconf.argv()
   .env()
   .file({file: 'config.json'})
 
-// THIS ENTIRE BLOCK IS NOT NECESSARY in PCF
-//   // populate mgmt api user
-// if (apigee.getMode() === apigee.APIGEE_MODE) {
-//   var vaultname = nconf.get('apigee_edge').authvault
-//   var vault = apigee.getVault(vaultname)
-//   vault.get('username', function (err, value) {
-//     if (err) {
-//       throw new Error('Unable to retrieve admin credentials. Check vault.')
-//     } else {
-//       nconf.set('apigee_edge:username', value)
-//       vault.get('password', function (err, value) {
-//         if (err) {
-//           throw new Error('Unable to retrieve admin credentials. Check vault.')
-//         } else {
-//           nconf.set('apigee_edge:password', value)
-//           console.log('set user/pass for edge mgmt api.')
-//         }
-//       })
-//     }
-//   })
-// } else {
-//   // TODO: fix this - temporary just to validate
-//   // get auth from cf environment
-// //  var creds = JSON.parse(process.env.apigee_credentials)
-// //  nconf.set('apigee_edge:username', creds.user)
-// //  nconf.set('apigee_edge:password', creds.pass)
-// }
-
 module.exports = nconf
