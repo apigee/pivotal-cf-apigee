abb3e2c215cb6e1efac9de812b56de9e7d3dab8a
JaredGordon <jgordon@pivotal.io>
2016-03-28 11:03:44 -0400

    include rest of app in zip.

--- apigee-cf-service-broker/ci/scripts/tile-build.sh
+++ apigee-cf-service-broker/ci/scripts/tile-build.sh
@@ -14,7 +14,7 @@ if [ -n "${HISTORY}" ]; then
 	cp ${HISTORY} ${SOURCE_DIR}/tile-history.yml
 fi
 
-(cd ${SOURCE_DIR}; zip -r resources/apigee.zip server.js package.json; $TILE build)
+(cd ${SOURCE_DIR}; zip -r resources/apigee.zip server.js package.json api helpers policy_templates proxy_resources schemas util; $TILE build)
 
 VERSION=`grep '^version:' ${SOURCE_DIR}/tile-history.yml | sed 's/^version: //'`
 HISTORY="tile-history-${VERSION}.yml"
