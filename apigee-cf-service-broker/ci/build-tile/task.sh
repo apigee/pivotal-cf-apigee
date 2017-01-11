acecfd780723e49eac66a214ec5631fc8c064ff8
JaredGordon <jgordon@pivotal.io>
2017-01-11 15:56:41 -0500

    add a sepparet build step

--- apigee-cf-service-broker/ci/build-tile/task.sh
+++ apigee-cf-service-broker/ci/build-tile/task.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -ex
 
-ver=`more ../../version/number`
+ver=`more version/number`
 (cd tile-repo/apigee-cf-service-broker; npm install; zip -r resources/apigee.zip EULA.txt LICENSE README.md manifest.yml api helpers policy_templates proxy-resources schemas util node_modules package.json server.js; tile build ${ver})
 
 file=`ls product/*.pivotal`
 filename=$(basename "${file}")
 filename="${filename%-*}"
 
-cp ${file} ../../broker-tile/${filename}-${ver}.pivotal
-cp tile-history.yml ../../tile-history-new/tile-history-${ver}.yml
+cp ${file} broker-tile/${filename}-${ver}.pivotal
+cp tile-history.yml tile-history-new/tile-history-${ver}.yml
