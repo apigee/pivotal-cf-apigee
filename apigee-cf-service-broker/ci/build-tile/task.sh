f5a9df12047a876d9fc42f9cb9442e1e5a4e27c5
JaredGordon <jgordon@pivotal.io>
2017-01-11 16:23:50 -0500

    fix paths

--- apigee-cf-service-broker/ci/build-tile/task.sh
+++ apigee-cf-service-broker/ci/build-tile/task.sh
@@ -3,12 +3,12 @@
 ver=`more version/number`
 
 cd tile-repo/apigee-cf-service-broker
-cp ../broker-zip/*.zip resources/
+cp ../../broker-zip/*.zip resources/
 tile build ${ver}
 
 file=`ls product/*.pivotal`
 filename=$(basename "${file}")
 filename="${filename%-*}"
 
-cp ${file} broker-tile/${filename}-${ver}.pivotal
-cp tile-history.yml tile-history-new/tile-history-${ver}.yml
+cp ${file} ../../broker-tile/${filename}-${ver}.pivotal
+cp tile-history.yml ../../tile-history-new/tile-history-${ver}.yml
